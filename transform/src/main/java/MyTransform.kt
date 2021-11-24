44da3d89a884426e7064907a10eeeebf7c45b04d
sweetwang <yuanlai5chaos@gmail.com>
2021-11-24 15:28:56 +0800

    完善Transform模板

--- transform/src/main/java/MyTransform.kt
+++ transform/src/main/java/MyTransform.kt
@@ -1,7 +1,14 @@
+import com.android.build.api.transform.Format
 import com.android.build.api.transform.QualifiedContent
 import com.android.build.api.transform.Transform
 import com.android.build.api.transform.TransformInvocation
+import com.android.build.gradle.internal.pipeline.TransformManager
+import com.android.utils.FileUtils
+import org.apache.commons.codec.digest.DigestUtils
 
+/**
+ * 参考 [Gradle Plugin: Transform + Javassist 编译期操作Class文件](https://www.jianshu.com/p/882325acd200)
+ */
 class MyTransform : Transform() {
 
     override fun getName(): String {
@@ -9,11 +16,11 @@ class MyTransform : Transform() {
     }
 
     override fun getInputTypes(): MutableSet<QualifiedContent.ContentType> {
-        return mutableSetOf(QualifiedContent.DefaultContentType.CLASSES)
+        return TransformManager.CONTENT_CLASS
     }
 
     override fun getScopes(): MutableSet<in QualifiedContent.Scope> {
-        return mutableSetOf(QualifiedContent.Scope.PROJECT)
+        return TransformManager.SCOPE_FULL_PROJECT
     }
 
     override fun isIncremental(): Boolean {
@@ -21,18 +28,38 @@ class MyTransform : Transform() {
     }
 
     override fun transform(invocation: TransformInvocation) {
-        println(invocation)
         val inputs = invocation.inputs
+        val outputProvider = invocation.outputProvider
+
         inputs.forEach { input ->
-            input.directoryInputs.forEach { directoryInput ->
-                directoryInput.file.walk().forEach { file ->
-                    println(file)
-                }
+            input.directoryInputs.forEach { dirInput ->
+                val dest = outputProvider.getContentLocation(
+                    dirInput.name,
+                    dirInput.contentTypes,
+                    dirInput.scopes,
+                    Format.DIRECTORY
+                )
+                FileUtils.copyDirectory(dirInput.file, dest)
             }
             input.jarInputs.forEach { jarInput ->
-                println(jarInput.file)
+                // 重命名输出文件（同目录 copyFile 会冲突）
+                var jarName = jarInput.name
+                if (jarName.endsWith(".jar")) {
+                    jarName = jarName.substring(0, jarName.length - 4)
+                }
+                val md5Name = DigestUtils.md5Hex(jarInput.file.absolutePath)
+
+                val dest = outputProvider.getContentLocation(
+                    jarName + md5Name,
+                    jarInput.contentTypes,
+                    jarInput.scopes,
+                    Format.JAR
+                )
+
+                FileUtils.copyFile(jarInput.file, dest)
             }
         }
+
     }
 
 }
